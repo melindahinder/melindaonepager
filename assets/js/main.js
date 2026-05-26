1793ad21b875fd856296e1b468225376187cc281
Melinda <melinda.hinder@bzwu.ch>
2026-05-26 10:58:44 +0200

    About Me Abschnitt fertig html&css und js animation für aboutMe h2 angepasst

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,33 @@
 /*Im JS gamcht das es auf allen Browsern funktionniert*/
 
 window.addEventListener('scroll', () => {
-  const sections = document.querySelectorAll('.section-block');
+  const titles = document.querySelectorAll('.section-title');
 
-  sections.forEach(section => {
-    const title = section.querySelector('.section-title');
+  titles.forEach(title => {
+    const section = title.closest('.section-block');
+    if (!section) return;
+
     const rect = section.getBoundingClientRect();
     const windowHeight = window.innerHeight;
 
     let progress = 1 - (rect.top / windowHeight);
     progress = Math.min(Math.max(progress, 0), 1);
 
-    let scale = 0.6 + progress * 0.6; // klein → groß
+    let scale;
+
+    // ABOUT ME soll weniger stark wachsen
+    if (title.classList.contains('about-title')) {
+            scale = 0.6 + progress * 0.2;  // wächst nur minimal
+    } 
+    // alle anderen Titel normal
+    else {
+      scale = 0.6 + progress * 0.6;    // dein normaler Effekt
+    }
+
     title.style.transform = `scale(${scale})`;
   });
 });
+
 
 
 
