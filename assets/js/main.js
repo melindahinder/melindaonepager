ea54d4d9120b0b8f1a36eaac433e252c7cc57c47
Melinda <melinda.hinder@bzwu.ch>
2026-05-26 08:37:40 +0200

    Galerie mit bento-grid und h2 gefixt mit der Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,20 @@
 /*Im JS gamcht das es auf allen Browsern funktionniert*/
 
 window.addEventListener('scroll', () => {
-  const concept = document.querySelector('.section-title');
-  const title = document.querySelector('.title');
+  const sections = document.querySelectorAll('.section-block');
 
-  const rect = concept.getBoundingClientRect();
-  const windowHeight = window.innerHeight;
+  sections.forEach(section => {
+    const title = section.querySelector('.section-title');
+    const rect = section.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
 
-  // Fortschritt berechnen (0 = oben, 1 = unten)
-  let progress = 1 - (rect.top / windowHeight);
-  progress = Math.min(Math.max(progress, 0), 1);
+    let progress = 1 - (rect.top / windowHeight);
+    progress = Math.min(Math.max(progress, 0), 1);
 
-  // Scale: klein → groß
-  let scale = 0.6 + progress * 0.6; // von 0.6 bis 1.2
-
-  title.style.transform = `scale(${scale})`;
+    let scale = 0.6 + progress * 0.6; // klein → groß
+    title.style.transform = `scale(${scale})`;
+  });
 });
+
 
 
